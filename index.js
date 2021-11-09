3892756cf62d77121b20977dd18b8998af2261b0
Ashton B <ashtonbrugh@gmail.com>
2021-11-09 10:54:30 -0500

    adding the time and date

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-moment().format('MMMM Do YYYY, h:mm:ss a');
+var NowMoment = moment();
+var eDisplayMoment = document.getElementById('currentDay');
+eDisplayMoment.innerHTML = NowMoment.format("dddd, MMMM Do YYYY, h:mm:ss:a");
